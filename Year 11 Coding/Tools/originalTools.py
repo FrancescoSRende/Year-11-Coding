486355dd508f1a9f275764cdb2291ca30a4ce6c4
FrancescoSRende <francesco.rende@ucc.on.ca>
2020-10-01 10:37:10 -0400

    added some proj euler tools

--- Year 11 Coding/Tools/originalTools.py
+++ Year 11 Coding/Tools/originalTools.py
@@ -180,7 +180,7 @@ print("")
 
 
 
-#Description: this function takes an int and returns its largest factor
+#Description: this function takes an int and returns its largest prime factor
 #Parameters: int
 #Returns: int
 #Precondition: a is an integer
@@ -411,6 +411,152 @@ def base2ToHex(s):
 
 print('base2ToHex')
 print(base2ToHex('1111'))
+
+
+
+print("")
+
+
+
+
+
+#Description: this function takes the three constants of a quadratic equation (a,b,c) and returns the zeroes of said quadratic (unless they are imaginary)
+#Parameters: three ints
+#Returns: 1 or 2 ints or a string if the zeroes are imaginary
+
+def solveQuadratic(a,b,c):
+
+    a = float(a)
+    b = float(b)
+    c = float(c)
+
+    if b**2 >= 4*a*c:
+
+        determinant = ( (b**2) - (4*a*c) ) ** 0.5
+
+        top_add = -b + determinant
+
+        top_subtract = -b - determinant
+
+        bottom = 2*a
+
+        zero_1 = top_add / bottom
+        zero_2 = top_subtract / bottom
+    
+
+        if zero_1 == zero_2:
+            return zero_1
+        else:
+            print (zero_1)
+            print (zero_2)
+            return
+    
+    else:
+        return "imaginary solutions"
+    
+
+print('solveQuadratic')
+print(solveQuadratic(1,1,-1))
+
+
+
+print("")
+
+
+
+
+#Description: this function takes a number and returns the greatest possible product using a certain number of its digits
+#Parameters: two ints
+#Returns: int
+
+def projEuler8ish(num,a):
+
+    a = str(a)
+
+    check_total = 0
+    check_9 = 0
+    check_8 = 0
+    check_7 = 0
+
+    product = 0
+
+
+    for i in range(0,len(a),1):
+        if a[i] == '9':
+            check_9 = check_9 + 1
+            check_total = check_total + 1
+
+    
+    if check_total <= num:    
+        for i in range(0,len(a),1):
+            if a[i] == '8':
+                check_8 = check_8 + 1
+                check_total = check_total + 1
+
+
+        if check_total <= num:
+
+            for i in range(0,len(a),1):
+                if a[i] == '7':
+                    check_7 = check_7 + 1
+                    check_total = check_total + 1
+
+
+            if check_total <= num:
+
+                for i in range(0,len(a),1):
+                    if a[i] == '6':
+                        check_6 = check_6 + 1
+                        check_total = check_total + 1
+
+                
+
+
+
+    product = (9**check_9) * (8**check_8) * (7**check_7)
+
+    return product
+    
+
+print('projEuler8ish')
+print(projEuler8ish(13,7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450))
+
+
+
+print("")
+
+
+
+
+
+
+#Description: this function takes an int and returns the sum of all primes less than it
+#Parameters: int
+#Returns: int
+
+
+#NEEDS WORK
+def sumPrimes(a):
+
+    primes = []
+    check = 0
+
+    for x in range(2,a,1):
+        half_kinda = round(float(x)/2) + 1
+        for i in range(2,half_kinda,1):
+            if x % i == 0 and x != i:
+                check = check + 1
+
+        print(str(x) + ", " + str(check))
+        
+        if check == 0:
+            primes.append(x)
+
+    return primes
+    
+
+print('sumPrimes')
+print(sumPrimes(17))
 
 
 
